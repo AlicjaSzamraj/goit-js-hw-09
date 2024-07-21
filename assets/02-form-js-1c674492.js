e5c3b6b50d07b1d8ecb1869ed7de0d75b66ba588
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-07-21 16:33:13 +0000

    Deploying to gh-pages from @ AlicjaSzamraj/goit-js-hw-09@d0797c17bfacbdf5ee665175b2d4ea5b70652423 🚀

--- assets/02-form-js-1c674492.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("form.feedback-form"),t=e.elements.email,a=e.elements.message;t.addEventListener("input",s);a.addEventListener("input",s);function s(){const n={email:t.value,message:a.value};localStorage.setItem("feedback-form-state",JSON.stringify(n))}
-//# sourceMappingURL=02-form-js-1c674492.js.map
