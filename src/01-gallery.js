29fc0692a0a9e38d33f31f292c40a3ca37918d2e
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-07-21 13:48:10 +0100

    create gallery with lightbox

---
+++ src/01-gallery.js
@@ -0,0 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import { images } from './images';
+
+const createImage = ({ preview, original, description }) => {
+  const li = document.createElement('li');
+  li.classList.add('gallery-item');
+
+  li.innerHTML = `<a class="gallery-link" href="${original}">
+    <img 
+    class="gallery-image"
+    src="${preview}"
+    alt="${description}"
+    />
+    </a>`;
+  return li;
+};
+
+const initImages = images => {
+  const imgs = images.map(createImage);
+
+  const fragment = document.createDocumentFragment();
+  fragment.append(...imgs);
+  return fragment;
+};
+
+const main = () => {
+  document.querySelector('.gallery').appendChild(initImages(images));
+};
+
+main();
+
+const lightbox = new SimpleLightbox('ul.gallery li', {
+  captionType: 'alt',
+});
